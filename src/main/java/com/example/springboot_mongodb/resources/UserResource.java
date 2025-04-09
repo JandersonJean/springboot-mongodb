89d1dd2f4beecadecb45fcb6bc9447ba1bcda31e
Janderson Jean <jandersonjean@gmail.com>
2025-04-09 07:10:37 -0300

    Obtendo um usuário por id e tratamento de exceção

--- src/main/java/com/example/springboot_mongodb/resources/UserResource.java
+++ src/main/java/com/example/springboot_mongodb/resources/UserResource.java
@@ -6,11 +6,13 @@ import com.example.springboot_mongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -24,5 +26,10 @@ public class UserResource {
         List<User> users = userService.findAll();
         List<UserDTO> userDTOs = users.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
         return ResponseEntity.ok().body(userDTOs);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
